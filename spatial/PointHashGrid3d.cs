1328b9e153166eb42e2701eb9fbe25c3ce3a94d0
Juan Florez <juan.florez@stratosphere-games.com>
2023-06-01 11:30:39 +0200

    feature: modify pointhashgrid3d invalidvalue access

--- spatial/PointHashGrid3d.cs
+++ spatial/PointHashGrid3d.cs
@@ -24,7 +24,7 @@ namespace g4
     {
         Dictionary<Vector3i, List<T>> Hash;
         protected ScaleGridIndexer3 Indexer;
-        T invalidValue;
+        protected T invalidValue;
 
         SpinLock spinlock;
 
